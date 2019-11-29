e5fcc44e80fac80b57da449ee9bd57bf4a1fd476
Rodolphe Pineau <pineau@rti-zone.org>
2019-11-28 16:09:02 -0800

    Adding enw buils script, update .gitignore

--- installer/build.sh
+++ installer/build.sh
@@ -6,12 +6,15 @@ cp "../ACE.png" ROOT/tmp/ACEDome_X2/
 cp "../domelist ACEDome.txt" ROOT/tmp/ACEDome_X2/
 cp "../build/Release/libACEDome.dylib" ROOT/tmp/ACEDome_X2/
 
+PACKAGE_NAME="ACEDome_X2.pkg"
+BUNDLE_NAME="org.rti-zone.ACEDomeX2"
+
 if [ ! -z "$installer_signature" ]; then
-# signed package using env variable installer_signature
-pkgbuild --root ROOT --identifier org.rti-zone.ACEDome_X2 --sign "$installer_signature" --scripts Scripts --version 1.0 ACEDome_X2.pkg
-pkgutil --check-signature ./ACEDome_X2.pkg
+	# signed package using env variable installer_signature
+	pkgbuild --root ROOT --identifier $BUNDLE_NAME --sign "$installer_signature" --scripts Scripts --version 1.0 $PACKAGE_NAME
+	pkgutil --check-signature ./${PACKAGE_NAME}
 else
-pkgbuild --root ROOT --identifier org.rti-zone.ACEDome_X2 --scripts Scripts --version 1.0 ACEDome_X2.pkg
+	pkgbuild --root ROOT --identifier $BUNDLE_NAME --scripts Scripts --version 1.0 $PACKAGE_NAME
 fi
 
 rm -rf ROOT
